6c3f384c55609c35a5c6879910814006c87d38fe
Manish Acharya <manuacharya54@gmail.com>
2025-11-05 13:11:14 +0545

    Initial Commit

---
+++ SoDo/SoDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoDoApp.swift
+//  SoDo
+//
+//  Created by manish-ach on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SoDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
